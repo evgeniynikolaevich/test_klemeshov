2dfd68877c41dae7a5c3fee024dab123e70a986a
evgesha <zplacesound@gmail.com>
2020-07-09 12:12:45 +0300

    with tests

--- backend.py
+++ backend.py
@@ -4,6 +4,12 @@ import json
 from django.db.models import Q
 from django.core import serializers
 from django.http import HttpResponse,JsonResponse
+from django.test import TestCase,Client
+import random
+from django.utils.crypto import get_random_string
+from django.template.defaultfilters import slugify
+from faker import Faker
+from django.urls import reverse
 
 #1
 class Product(models.Model):
@@ -39,3 +45,34 @@ def some_another_live_search(request, template_name="shop/livesearch_results.htm
 
 #some tests
 #2
+
+
+
+class BasicTest(TestCase):
+    def test_fields(self):
+        fake = Faker()
+        categ = Category.objects.last()
+        related_products = Product.objects.last()
+        item = Product()
+        item.sku = get_random_string(length=32)
+        item.price = random.randint(1000,10000)
+        item.name = fake.name()
+        item.slug = slugify(item.name)
+        item.title = fake.address()
+        item.description = fake.text()
+        item.save()
+        record = Product.objects.get(name = item.name) 
+        self.assertEqual(record,item)
+    
+
+    def setUp(self):
+        self.client = Client()
+        self.list_url = reverse('live_search')
+
+
+
+    def test_live_search(self):
+        response = self.client.get(reverse(self.list_url))
+        self.assertEqual(response.status_code,200)
+         
+
